2bbc3def6c6fa406658324a0463cade047c1d187
NathanPhippsONeill <nathanphippsoneill@gmail.com>
2021-01-18 21:15:28 +0000

    Data set change: Changed to left right data set

--- main_importHRTF.py
+++ main_importHRTF.py
@@ -1,17 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-HRTF_path = (
-  "/home/phippsy/Documents/BinauralMastersProject/compact/elev-10/H-10e090a.dat"
+HRTF_L_path = (
+  "/home/phippsy/masters-proj/full/elev-10/L-10e090a.dat"
+)
+HRTF_R_path = (
+  "/home/phippsy/masters-proj/full/elev-10/R-10e090a.dat"
 )
 
 # i is signed integer
 # u is unsigned integer
 # > is big endian, < is little endian
 # The number after specifies the number of bytes (e.g. 2 = 2 bytes / 16 bits)
-data = np.fromfile(HRTF_path, dtype='>i2') # big-endian signed 16 bit integer 
-
-leftimp = data[0:256:2]
-rightimp = data[1:256:2]
+leftimp = np.fromfile(HRTF_L_path, dtype='>i2') # big-endian signed 16 bit integer 
+rightimp = np.fromfile(HRTF_L_path, dtype='>i2') #big-endian signed 16 bit integer 
 
 print(leftimp)
 print(rightimp)
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(leftimp)
+
+plt.subplot(2, 1, 2)
+plt.plot(rightimp)
+
+
+plt.show()
